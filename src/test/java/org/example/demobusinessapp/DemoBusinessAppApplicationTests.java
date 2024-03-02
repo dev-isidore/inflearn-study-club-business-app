50a13b34bae05ae0e9691ac4c185211d9327a600
MinKyu Song <dev.isidore@gmail.com>
2024-03-02 15:55:44 +0900

    initial commit

---
+++ src/test/java/org/example/demobusinessapp/DemoBusinessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demobusinessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBusinessAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
